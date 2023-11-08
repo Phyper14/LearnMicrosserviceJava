5270c31f6c9a74e0036ee749bfa81ae34533dc6d
Phyper14 <martins14.pablo@gmail.com>
2023-11-08 14:12:26 -0300

    feat: ms-user: adding a producer to generete the messages

--- email/src/main/java/com/ms/email/dtos/EmailRecordDto.java
+++ email/src/main/java/com/ms/email/dtos/EmailRecordDto.java
@@ -2,5 +2,5 @@ package com.ms.email.dtos;
 
 import java.util.UUID;
 
-public record EmailRecordDto(UUID userId, String emailTo, String subject, String message) {
+public record EmailRecordDto(UUID userId, String emailTo, String subject, String text) {
 }
